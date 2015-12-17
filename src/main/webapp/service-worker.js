9898179d0a724d7cb8710575893adbf987fa12d7
DrBAXA <DrBAXA@hotmail.com>
2015-12-17 08:42:51 +0200

    Use ndexDB with Dexie.js to save userId in worker

--- src/main/webapp/service-worker.js
+++ src/main/webapp/service-worker.js
@@ -1,5 +1,5 @@
-
 self.addEventListener('push', function (event) {
+    createDb();
     getMessages(show)
 });
 
@@ -9,15 +9,18 @@ self.addEventListener('notificationclick', function (event) {
 });
 
 self.addEventListener('message', function (evt) {
-    self.userId = evt.data.id;
+    createDb();
+    storeUserId(self.db, evt.data.id);
     console.log('postMessage received', evt.data);
 });
 
 function setRead(id){
-    fetch('users/' + self.userId + '/messages/v1/'+id, {
-        method: "POST"
-    }).catch(function(e){
-        console.log(e);
+    getUserId(self.db, function(userId){
+        fetch('users/' + userId + '/messages/v1/'+id, {
+            method: "POST"
+        }).catch(function(e){
+            console.log(e);
+        });
     });
 }
 
@@ -26,25 +29,46 @@ function show(title, notif) {
 }
 
 function getMessages(calback) {
-    fetch('users/' + self.userId + '/messages/v1/unread')
-        .then(
-        function (data) {
-            data.json().then(function (messages) {
-                messages.forEach(function (message) {
-                    var title = message.title;
-                    var body = message.message;
-                    var icon = message.icon;
+    getUserId(self.db, function(userId){
+        fetch('users/' + userId + '/messages/v1/unread')
+            .then(
+            function (data) {
+                data.json().then(function (messages) {
+                    messages.forEach(function (message) {
+                        var title = message.title;
+                        var body = message.message;
+                        var icon = message.icon;
 
-                    calback(title, {
-                        body: body,
-                        icon: icon,
-                        data: message.id
+                        calback(title, {
+                            body: body,
+                            icon: icon,
+                            data: message.id
+                        });
                     });
                 });
+            })
+            .catch(function (err) {
+                console.log('Fetch Error :-S', err);
             });
-        })
-        .catch(function (err) {
-            console.log('Fetch Error :-S', err);
-        });
+    });
+}
+
+function createDb(){
+    if(! self.db) {
+        importScripts("./Dexie.js");
+        self.db = new Dexie("pushDatabase");
+        db.version(1).stores({values: "name, value"});
+        db.open();
+    }
+}
+
+function getUserId(db, callback){
+    db.values.where("name").equals("userId").first(function(item){
+        callback(item.value);
+    })
+}
+
+function storeUserId(db, userId){
+    db.values.put({name: "userId", value: userId});
 }
 
