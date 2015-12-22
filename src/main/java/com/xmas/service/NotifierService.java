17a4cdc70dc357c20a9f05f18ad2837872df7698
vdanyliuk <vdanyliuk@softjourn.com>
2015-12-22 18:11:25 +0200

    scheduller

--- src/main/java/com/xmas/service/NotifierService.java
+++ src/main/java/com/xmas/service/NotifierService.java
@@ -5,9 +5,12 @@ import com.xmas.entity.Message;
 import com.xmas.notifiers.chrome.ChromeNotifier;
 import com.xmas.notifiers.safari.SafariNotifier;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.ScheduledExecutorService;
 
 @Service
 public class NotifierService {
@@ -18,10 +21,17 @@ public class NotifierService {
     @Autowired
     SafariNotifier safariNotifier;
 
+    @Autowired
+    ScheduledExecutorService executor;
+
     public void push(Medium medium, Message message, List<String> tokens){
         switch (medium.getName()){
             case Medium.CHROME: chromeNotifier.pushMessage(message, tokens);break;
             case Medium.SAFARI: safariNotifier.pushMessage(message, tokens);break;
         }
+    }
+
+    public void postponedPush(Medium medium, Message message, List<String> tokens, LocalDateTime time){
+        executor.schedule()
     }
 }
