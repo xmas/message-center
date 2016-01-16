06921d9ee4f8b893dd4dd2d0108bac491cef5a1e
DrBAXA <DrBAXA@hotmail.com>
2016-01-16 15:15:02 +0200

    Answer controller

--- src/main/java/com/xmas/dao/questions/AnswerRepository.java
+++ src/main/java/com/xmas/dao/questions/AnswerRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface AnswerRepository extends CrudRepository<Answer, Long>{
 
     @Query("SELECT answer FROM Answer answer WHERE answer.question = ?1 AND answer.date = ?2")
     Optional<Answer> findAnswer(Question question, LocalDate date);
+
+    @Query("SELECT answer FROM Answer answer WHERE answer.question = ?1 AND (answer.date BETWEEN ?2 AND ?3)")
+    List<Answer> findAnswers(Question question, LocalDate from, LocalDate to);
 }
