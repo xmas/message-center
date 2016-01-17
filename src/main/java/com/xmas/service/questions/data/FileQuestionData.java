fff3bec1d9dd8f400d618a01deffcc0496f79b6f
DrBAXA <DrBAXA@hotmail.com>
2016-01-17 10:47:43 +0200

    Save previous files for  questions in dir based on ful time

--- src/main/java/com/xmas/service/questions/data/FileQuestionData.java
+++ src/main/java/com/xmas/service/questions/data/FileQuestionData.java
@@ -9,18 +9,22 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.nio.file.StandardOpenOption.*;
 
 public class FileQuestionData {
 
     private static final Logger logger = LogManager.getLogger();
 
     public static final String INPUT_FILE_NAME = "input.dat";
+    public static final String INFO_FILE_NAME = ".info";
 
     private String dataDirPath;
 
@@ -52,6 +56,7 @@ public class FileQuestionData {
         } else {
             packagePreviousFiles(questionDir);
         }
+        addInfoFile(dataDirPath);
     }
 
     private void packagePreviousFiles(File directory) {
@@ -67,7 +72,7 @@ public class FileQuestionData {
     }
 
     private File createDailyPackageDir(File parentDir) {
-        String dailyDirName = LocalDate.now().minusDays(1).format(DateTimeFormatter.ofPattern("ddMMyyyy"));
+        String dailyDirName = getCreatedDateTime(parentDir.getAbsolutePath());
 
         File dailyDr = parentDir.toPath().resolve(dailyDirName).toFile();
 
@@ -80,16 +85,43 @@ public class FileQuestionData {
     }
 
     private void moveFiles(List<File> files, File destinationDirectory) {
-        files.stream().forEach(file -> {
-            try {
-                Files.move(file.toPath(),
-                        destinationDirectory
-                                .toPath()
-                                .resolve(file.toPath().getFileName().toString()),
-                        StandardCopyOption.REPLACE_EXISTING);
-            } catch (IOException e) {
-                throw new ProcessingException("Can't move old files");
-            }
-        });
+        files.stream()
+                .filter(file -> !file.toPath().getFileName().toString().equals(INFO_FILE_NAME))
+                .forEach(file -> moveFile(file, destinationDirectory));
+    }
+
+    private void moveFile(File file, File destinationDirectory){
+        try {
+            Files.move(file.toPath(),
+                    destinationDirectory
+                            .toPath()
+                            .resolve(file.toPath().getFileName().toString()),
+                    StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new ProcessingException("Can't move old files");
+        }
+    }
+
+    private void addInfoFile(String questionDir){
+        Path infoFile = new File(questionDir).toPath().resolve(INFO_FILE_NAME);
+        try {
+            String data = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy_HH-mm-ss"));
+            Files.write(infoFile, data.getBytes(), WRITE, TRUNCATE_EXISTING, CREATE);
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+            logger.debug(e.getMessage(), e);
+            throw new ProcessingException("Can't write to info file " + infoFile);
+        }
+    }
+
+    private String getCreatedDateTime(String questionDir){
+        Path infoFile = new File(questionDir).toPath().resolve(INFO_FILE_NAME);
+        try {
+            return new String(Files.readAllBytes(infoFile));
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+            logger.debug(e.getMessage(), e);
+            throw new ProcessingException("Can't read from info file " + infoFile);
+        }
     }
 }
