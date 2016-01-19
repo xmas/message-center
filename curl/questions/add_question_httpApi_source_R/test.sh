e63f75f8ac1a0ab8191a71011740ed7615478448
DrBAXA <DrBAXA@hotmail.com>
2016-01-19 23:08:59 +0200

    run script every day

--- curl/questions/add_question_httpApi_source_R/test.sh
+++ curl/questions/add_question_httpApi_source_R/test.sh
@@ -6,7 +6,7 @@ curl -i -X POST -H "Content-Type: multipart/form-data"  \
   -F "dataSourceResource=https://schemata.io/query/query?queryToken=PSLXNBJ9EJPIJM2I1GRSKEPBVGF4H338HY6XSN3Q" \
   -F "scriptType=R" \
   -F "dataType=FILE" \
-  -F "cron=20 * * * * ?" \
+  -F "cron=0 0 3 * * ?" \
   -F "tags=tag3" \
   -F "tags=tag2" \
   localhost:8080/push/questions
