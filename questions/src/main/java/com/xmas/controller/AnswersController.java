78a4aa1174e2a23169e6be15ffedd355663b0755
DrBAXA <DrBAXA@hotmail.com>
2016-02-20 19:42:27 +0200

    Get answers by tags

--- questions/src/main/java/com/xmas/controller/AnswersController.java
+++ questions/src/main/java/com/xmas/controller/AnswersController.java
@@ -3,6 +3,7 @@ package com.xmas.controller;
 import com.xmas.dao.AnswerRepository;
 import com.xmas.dao.QuestionRepository;
 import com.xmas.entity.Answer;
+import com.xmas.entity.Tag;
 import com.xmas.exceptions.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -13,9 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/{qId}/answers")
+@RequestMapping("/")
 public class AnswersController {
 
     @SuppressWarnings("SpringJavaAutowiringInspection")
@@ -26,8 +29,7 @@ public class AnswersController {
     @Autowired
     private QuestionRepository questionRepository;
 
-
-    @RequestMapping
+    @RequestMapping("/{qId}/answers")
     public Iterable<Answer> getAnswers(@PathVariable Long qId,
                                        @RequestParam(required = false)
                                        @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) LocalDate from,
@@ -40,11 +42,24 @@ public class AnswersController {
                 .orElseThrow(() -> new NotFoundException("There is no question with such id"));
     }
 
+    @RequestMapping("/answers")
+    public Iterable<Answer> getAnswers(@RequestParam List<Tag> tags,
+                                       @RequestParam(required = false)
+                                       @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) LocalDate from,
+                                       @RequestParam(required = false)
+                                       @DateTimeFormat(iso= DateTimeFormat.ISO.DATE)LocalDate to) {
+        LocalDateTime startFromDay = getStartOfDay(from);
+        LocalDateTime endToDay = getEndOfDay(to);
+        return questionRepository.getByTags(tags).stream()
+                .flatMap(q -> answerRepository.findAnswers(q, startFromDay, endToDay).stream())
+                .collect(Collectors.toList());
+    }
+
     private LocalDateTime getStartOfDay(LocalDate date){
         return date == null ? LocalDate.now().atStartOfDay() : date.atStartOfDay();
     }
 
     private LocalDateTime getEndOfDay(LocalDate date){
-        return date == null ? LocalDate.now().atTime(23, 59, 59) : date.atTime(23, 59, 59);
+        return (date == null ? LocalDate.now() : date).atTime(23, 59, 59);
     }
 }
